9027a1d22779946da6753fe9e782f49f5e62b957
Abhishek <abhishekdhar1991@gmail.com>
2022-05-03 00:51:41 +0530

    commit on Master

--- main.py
+++ main.py
@@ -7,4 +7,9 @@ app = FastAPI()
 
 @app.get("/")  # decorator
 def root():
-    return {"message": "Hello world!"}
+    return {"message": "Welcome to my API!!!"}
+
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is your post"}
