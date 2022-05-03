2e57ab3e742e468297bfe94264927aab43c27f64
Abhishek <abhishekdhar1991@gmail.com>
2022-05-03 20:56:52 +0530

    Commit from VS Code

--- main.py
+++ main.py
@@ -16,6 +16,10 @@ class Post(BaseModel):
     rating: Optional[int] = None
 
 
+my_posts = [{"title": "title of post 1",
+             "content": "content of of post 1", "id": 1}, {"title": "favourite NBA team", "content": "I am a fan of Boston Celtics", "id": 2}]
+
+
 @app.get("/")  # decorator
 def root():
     return {"message": "Welcome to my API!!!"}
@@ -23,10 +27,10 @@ def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is your post"}
+    return {"data": my_posts}
 
 
-@app.post("/createposts")
+@app.post("/posts")
 def create_posts(post: Post):
     print(post)
     print(post.dict())
