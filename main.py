c5dfbbe8e0c3bac382b19a225c318a9a0d6c7256
Abhishek <abhishekdhar1991@gmail.com>
2022-05-06 20:44:19 +0530

    commit from VS Code

--- main.py
+++ main.py
@@ -20,6 +20,12 @@ my_posts = [{"title": "title of post 1",
              "content": "content of of post 1", "id": 1}, {"title": "favourite NBA team", "content": "I am a fan of Boston Celtics", "id": 2}]
 
 
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
+
+
 @app.get("/")  # decorator
 def root():
     return {"message": "Welcome to my API!!!"}
@@ -36,3 +42,9 @@ def create_posts(post: Post):
     post_dict['id'] = randrange(0, 1000000)
     my_posts.append(post_dict)
     return {"data": post_dict}
+
+
+@app.get("/posts/{id}")
+def get_post():
+    post = find_post(id)
+    return{"post_detail": post}
